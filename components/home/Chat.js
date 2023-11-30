210179b773f8bf0fcb49d3fcb311eb9ae789b20b
galaxone <kunaldangi252@gmail.com>
2023-12-01 00:27:04 +0530

    Added delete chat feature!

--- components/home/Chat.js
+++ components/home/Chat.js
@@ -1,27 +1,34 @@
 import Link from "next/link";
 import { MoreHorizontal, Trash2 } from "lucide-react";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger} from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
 
 export default function Chat({ currentChatId, chatId, title, path }) {
+    let isDeleting = false;
+    const router = useRouter();
     if(title.length > 25){
         title = title.slice(0, 22);
         title = title + "...";
     }
 
     async function deleteChat() {
-        // const response = await fetch("/api/chat/delete", {
-        //     method: "POST",
-        //     body: JSON.stringify({
-        //         chatId: chatId
-        //     })
-        // });
-        // const data = await response.json();
-        // console.log(data);
-        
+        if(isDeleting) return;
+        isDeleting = true;
+        const response = await fetch("/api/chat/delete", {
+            method: "POST",
+            body: JSON.stringify({
+                chatId: chatId
+            })
+        });
+        const data = await response.json();
+        if(data.error) return console.log(data.error);
+        if(data.state){
+            router.push("/");
+        }
     }
 
     return (<div className="flex m-2 p-2 justify-between items-center rounded-md hover:bg-slate-700">
-        <Link href={path}>
+        <Link className="grow" href={path}>
             {title}
         </Link>
         {
