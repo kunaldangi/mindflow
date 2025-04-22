79c99c3945f29a5d50a5fdfb0f256d2dbeeffd9e
Kunal Dangi <kunaldangi252@gmail.com>
2025-04-22 23:36:00 +0530

    chore: added nodemon for development

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const dev = process.env.NODE_ENV !== 'production';
 
 const app = express();
 
-initializeRoutes(app); // initializing API routes
+async function main(){
+    initializeRoutes(app); // initializing API routes
+    
+    const nextHandler = await nextApp(dev); // initializing Next.js
+    app.use(nextHandler); // adding Next.js to Express
+    
+    app.listen(port, () => {
+        console.log(`Mindflow is running on PORT: ${port}`);
+    });
+}
 
-const nextHandler = await nextApp(dev); // initializing Next.js
-app.use(nextHandler); // adding Next.js to Express
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+main();
