becd9bdf274aa9b04c56000cc7cbb6d98c6bf8ce
Kunal Dangi <kunaldangi252@gmail.com>
2025-04-24 14:45:06 +0530

    feat: added session token verification middleware

---
+++ src/middleware/session.js
@@ -0,0 +1,23 @@
+import verifyToken from "../utils/verifyToken.js";
+
+let secured_routes = [
+    "/api/user",
+];
+
+export default async function checkSession(req, res, next){
+    for(let i = 0; i < secured_routes.length; i++){
+        let route = secured_routes[i];
+
+        if(req.path.startsWith(route)){
+            let session_token = req.cookies.session;
+            if(!session_token) return res.send(JSON.stringify({ error: "Invalid Session Token!" }));
+            
+            let session_data = await verifyToken(session_token, process.env.JWT_SESSION_SECRET);
+            if(!session_data) return res.send(JSON.stringify({ error: "Invalid Session!" }));
+            req.session = session_data;
+        }
+    }
+    next();
+}
+
+export { secured_routes };
