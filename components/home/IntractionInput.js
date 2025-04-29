3b80eb0ec8b1aa021dcd812914c3b3794b8b2e14
Kunal Dangi <kunaldangi252@gmail.com>
2025-04-29 23:36:28 +0530

    refactor: fixed old IntractionInput to work with new api endpoint

--- components/home/IntractionInput.js
+++ components/home/IntractionInput.js
@@ -1,130 +1,186 @@
 "use client";
 import "./IntractionInput.css";
-import { useCompletion } from 'ai/react';
+import { useEffect, useState, useRef } from "react";
+
 import { Loader2 } from "lucide-react"
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { SendHorizonal } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+
+let isFirstMsgSent = false; // used because nextjs renders the component twice in dev mode
 
 export default function IntractionInput({ chatId, messages, setMessages }) {
-    
-    let [newMessage, setNewMessage] = useState(null); // new message to be added in messages array, after creating a message
-    let [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [input, setInput] = useState("");
 
-    const {
-        completion, // (store the response and after finish store null) Type: string (or null if there's no result)
-        input, // (message or prompt to send to the API) Type: string
-        stop, // (stops the request) Type: function (For Future Use)
-        isLoading, // ( true when the request is loading, false when it's done or not) Type: boolean
-        handleInputChange, // (sets the value of 'input') Type: function
-        handleSubmit, // (sends the request) Type: function
-    } = useCompletion({
-        api: '/api/chat',
-        body: {
-            chat_id: chatId
-        },
-        onFinish: async (prompt, completion) => { // To save the response in database
-            let response = await fetch('/api/chat/messages/update', {
+    useEffect(() => {
+        if (messages?.length == 1 && messages[0]?.authorType == "user") {
+            if (isFirstMsgSent) return;
+            isFirstMsgSent = true;
+            console.log("Messages: ", messages);
+            console.log("First message from user: ", messages[0]?.content);
+            sendFirstMessage(messages[0]?.content);
+        }
+    }, [messages]);
+
+    const lastMessageRef = useRef(null);
+
+    async function sendFirstMessage() {
+        if (messages?.length == 1 && messages[0]?.authorType == "user") {
+            setLoading(true);
+
+            const response = await fetch('/api/ai', {
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ chat_id: parseInt(chatId), ai_response: completion })
+                headers: { 'Content-Type': 'application/json' },
+                credentials: 'include',
+                body: JSON.stringify({ chatId, model: "meta-llama/llama-4-scout:free", message: messages[0]?.content, isFirstMsg: true })
             });
-            console.log(prompt, completion);
-            let data = await response.json();
 
-            if (data.error) {
-                setNewMessage(null);
-                return console.log(data.error);
+            if (response.ok) {
+                const reader = response.body.getReader();
+                const decoder = new TextDecoder();
+    
+                const message = {
+                    id: Date.now(),
+                    chatId: chatId,
+                    authorType: "ai",
+                    authorId: 2,
+                    reasoning: "",
+                    content: "",
+                    createdAt: new Date().toISOString(),
+                    updatedAt: new Date().toISOString()
+                };
+    
+                setMessages(prev => {
+                    const updated = [...prev, message];
+                    lastMessageRef.current = message; // store reference
+                    return updated;
+                });
+    
+                while (true) {
+                    const { done, value } = await reader.read();
+                    if (done) break;
+    
+                    const chunk = decoder.decode(value, { stream: true });
+                    const lines = chunk.split('\n\n'); // Split at double newline
+    
+                    for (const line of lines) {
+                        if (line.startsWith('data: ')) {
+                            const jsonString = line.slice(6); // Remove 'data: '
+                            const aiMessage = JSON.parse(jsonString);
+    
+                            if (lastMessageRef.current) {
+                                aiMessage.reasoning ? lastMessageRef.current.reasoning += aiMessage.reasoning : null;
+                                aiMessage.content ? lastMessageRef.current.content += aiMessage.content : null;
+                                lastMessageRef.current.updatedAt = new Date().toISOString();
+                            }
+    
+                            setMessages(prev => {
+                                const updated = [...prev];
+                                updated[updated.length - 1] = { ...lastMessageRef.current };
+                                return updated;
+                            });
+                        }
+                    }
+                }
             }
-            if (data.state) {
-                setNewMessage(null);
+
+            setLoading(false);
+            setInput("");
+        }
+    }
+
+    async function sendMessage(e) {
+        setLoading(true);
+
+        const response = await fetch('/api/ai', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+            body: JSON.stringify({ chatId, model: "meta-llama/llama-4-scout:free", message: input, isFirstMsg: false })
+        });
+
+        if (response.ok) {
+            const reader = response.body.getReader();
+            const decoder = new TextDecoder();
+
+            const userMessage = {
+                id: Date.now(),
+                chatId: chatId,
+                authorType: "user",
+                authorId: 1,
+                reasoning: "",
+                content: input,
+                createdAt: new Date().toISOString(),
+                updatedAt: new Date().toISOString()
+            }
+
+            setMessages([...messages, userMessage]);
+
+            const message = {
+                id: Date.now(),
+                chatId: chatId,
+                authorType: "ai",
+                authorId: 2,
+                reasoning: "",
+                content: "",
+                createdAt: new Date().toISOString(),
+                updatedAt: new Date().toISOString()
+            };
+
+            setMessages(prev => {
+                const updated = [...prev, message];
+                lastMessageRef.current = message; // store reference
+                return updated;
+            });
+
+            while (true) {
+                const { done, value } = await reader.read();
+                if (done) break;
+
+                const chunk = decoder.decode(value, { stream: true });
+                const lines = chunk.split('\n\n'); // Split at double newline
+
+                for (const line of lines) {
+                    if (line.startsWith('data: ')) {
+                        const jsonString = line.slice(6); // Remove 'data: '
+                        const aiMessage = JSON.parse(jsonString);
+
+                        if (lastMessageRef.current) {
+                            aiMessage.reasoning ? lastMessageRef.current.reasoning += aiMessage.reasoning : null;
+                            aiMessage.content ? lastMessageRef.current.content += aiMessage.content : null;
+                            lastMessageRef.current.updatedAt = new Date().toISOString();
+                        }
+
+                        setMessages(prev => {
+                            const updated = [...prev];
+                            updated[updated.length - 1] = { ...lastMessageRef.current };
+                            return updated;
+                        });
+                    }
+                }
             }
         }
-    });
-    
-    const router = useRouter();
-    let sendMsg = chatId ? sendMessage : createNewChat; // if chat is present then send message in chat else create new chat :)
+
+        setLoading(false);
+        setInput("");
+    }
 
     let holdKey = null;
     function onKeyPress(e) {
-        if (holdKey != "Shift" && e.key == "Enter" && !loading) { sendMsg(e); }
+        if (holdKey != "Shift" && e.key == "Enter" && !loading) { sendMessage(e); }
         if (!holdKey) holdKey = e.key;
     }
     function onKeyRelease(e) {
         if (holdKey == e.key) holdKey = null;
     }
 
-    async function createNewChat() {
-        setLoading(true);
-
-        let response = await fetch('/api/chat/create', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ message: input })
-        });
-
-        let data = await response.json();
-
-        if (data.error) {
-            setLoading(false);
-            return console.log(data.error);
-        }
-
-        if (data.state) {
-            setLoading(false);
-            console.log(data);
-            router.push(`/chats/${data.chat_id}`);
-        }
-    }
-
-    async function sendMessage(e) {
-        setLoading(true);
-        let response = await fetch('/api/chat/messages/create', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ chat_id: chatId, message: input })
-        });
-        let data = await response.json();
-        if (data.error) {
-            setLoading(false);
-            return console.log(data.error);
-        }
-
-        if(data.response){
-            const updatedMessages = [...messages, data.response];
-            setMessages(updatedMessages);
-            setNewMessage(data.response);
-            handleSubmit(e); // Get stream response from AI
-            setLoading(false);
-        }
-    }
-    
-    useEffect(()=> {
-        if(messages && newMessage){
-            newMessage.ai_response = completion;
-            for(let i=0; i<messages.length; i++){
-                if(messages[i].id == newMessage.id){
-                    setMessages([...messages.slice(0, i), newMessage, ...messages.slice(i+1)]);
-                    break;
-                }
-            }
-        }
-    }, [completion, messages, newMessage, setMessages]);
-
     return (
         <div>
             <div className="flex flex-row items-start p-4 space-x-4">
-                <Textarea id="id-inp-intraction" onKeyDown={onKeyPress} onKeyUp={onKeyRelease} onChange={handleInputChange} className="h-[25px] max-h-[100px] px-5 py-4 text-[16px] font-[425] bg-transparent border-gray-400 rounded-[23px] text-white resize-none" placeholder="Message mindflow..." disabled={loading || isLoading} />
-                <Button onClick={sendMsg} className="self-center" disabled={loading || isLoading}>
-                    { loading || isLoading ? <Loader2 className="h-4 w-4 animate-spin" /> : <SendHorizonal className="h-4 w-4" /> }
+                <Textarea id="id-inp-intraction" value={input} onKeyDown={onKeyPress} onKeyUp={onKeyRelease} onChange={(e) => setInput(e.target.value)} className="h-[25px] max-h-[100px] px-5 py-4 text-[16px] font-[425] bg-transparent border-gray-400 rounded-[23px] text-white resize-none" placeholder="Message mindflow..." disabled={loading} />
+                <Button onClick={sendMessage} className="self-center" disabled={loading}>
+                    {loading ? <Loader2 className="h-4 w-4 animate-spin" /> : <SendHorizonal className="h-4 w-4" />}
                 </Button>
             </div>
         </div>
